0fc2ab02e3615225ed7dec1f28c1da1583d3e4bf
bui <bungbui@gmail.com>
2017-04-16 10:12:56 +0700

    now can quit

--- baba.go
+++ baba.go
@@ -7,18 +7,29 @@ import (
 )
 
 func main() {
-	fmt.Println("welcome to my test program. please tell me your name and age (if you dont tell your age or name you will cannot use if you say quit it will go out)")
+	fmt.Println("welcome to my test program.\n" +
+		"please tell me your name and age\n" +
+		"(if you dont tell your age or name you will not be able to use\n" +
+		"if you say quit, the program will be close,this program made by Boon")
 	var name string
 	var number int
-	fmt.Scanf("%s %d", &name, &number)
+	fmt.Scanf("%s", &name)
+
+	if name == "quit" {
+		fmt.Println("this process finish")
+		return
+	}
+
+	fmt.Scanf("%d", &number)
+
 	if number < 10 {
 		fmt.Println("you dont have permisson to use")
 	} else if number >= 10 && number < 80 {
 		fmt.Println("ok,", name, " can use this")
 		fmt.Println("password please")
-		var ABC string
-		fmt.Scanln(&ABC)
-		if ABC == "Boon" && (name == "Bui" || name == "Boo"  || name == "BoonBoon"){fmt.Println("wait")
+		var Password string
+		fmt.Scanln(&Password)
+		if Password == "Boon" && (name == "Bui" || name == "Boo"  || name == "Boon"){fmt.Println("wait")
 			_, err := exec.Command("Steam.exe").Output()
 			if err != nil {
 				fmt.Errorf("error; %s", err)
